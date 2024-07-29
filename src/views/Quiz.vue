27677eec11e30702ac7a16e64100239c1dfa87e8
Jibalfa <najibalfarizi5@gmail.com>
2024-07-29 11:53:47 +0700

    menampilkan daftar pertanyaan dan pilihan

--- src/views/Quiz.vue
+++ src/views/Quiz.vue
@@ -1,12 +1,23 @@
 <script setup>
 import QuizHeader from "@/components/QuizHeader.vue";
 import QuizContent from "@/components/QuizContent.vue";
+import { useRoute } from "vue-router";
+import quizes from "../data/quizes.json";
+import { ref } from "vue";
+
+const route = useRoute();
+
+const quizId = parseInt(route.params.id);
+const quiz = quizes.find((q) => q.id === quizId);
+
+const curretQuestionIndex = ref(0);
+
 </script>
 
 <script setup></script>
 <template>
   <QuizHeader />
-  <QuizContent />
+  <QuizContent :question="quiz.questions[curretQuestionIndex]" />
 </template>
 
 <style scoped></style>
